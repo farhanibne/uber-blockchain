b19db8546f5840d68613e624ce832eb92d8208f4
farhan ibne saif <84982846+farhanibne@users.noreply.github.com>
2022-03-06 16:29:07 +0600

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,29 @@
+import Navbar from '../components/Navbar'
+import Map from '../components/Map'
+import LocationSelector from '../components/LocationSelector'
+import Confirm from '../components/Confirm'
+
+const style = {
+  wrapper: `h-screen w-screen flex flex-col`,
+  main: `h-full w-screen flex-1 z-10`,
+  mapContainer: `flex-1 w-full h-full`,
+  rideRequestContainer: `h-full w-[400px] ml-[1rem] py-[3rem] absolute top-0 left-0 flex flex-col justify-end z-20`,
+  rideRequest: `h-full max-h-[700px] bg-white rounded-lg flex flex-col overflow-scroll`,
+}
+
+export default function Home() {
+  return (
+    <div className={style.wrapper}>
+      <Navbar />
+      <div className={style.main}>
+        <Map />
+      </div>
+      <div className={style.rideRequestContainer}>
+        <div className={style.rideRequest}>
+          <LocationSelector />
+          <Confirm />
+        </div>
+      </div>
+    </div>
+  )
+}
